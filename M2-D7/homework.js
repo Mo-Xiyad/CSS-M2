bb758fba36bbbbc9befdff7d113b3e499413e1ef
Zee <xziyad92@gmail.com>
2021-08-31 21:16:11 +0200

    Q19 js

--- M2-D7/homework.js
+++ M2-D7/homework.js
@@ -1,10 +1,5 @@
 /*
       JS Exercises
-
-      EX16) Write a function to remove the "Search" magnifying glass icon
-      EX17) Write a function to trim just the first 50 characters in the first paragraph for each blog post
-      EX18) Write a function and attach it to the "Newer" button, to add new Blog Post (just div and title)
-      EX19) Write a function and attach it to the "Older" button, to remove the last Blog Post
       EX20) Write an alert with the name of the author every time the user hover with the mouse over an author name
   */
 // EX11) Write a function to add a new link into the navbar
@@ -42,9 +37,64 @@ const removelinksElsewhere = function () {
 
 // EX15) Write a function to change the column size for heading in jumbotron
 
+const changeColSize = function () {
+    let manDiv = document.querySelector('div.jumbotron')
+    // let manDiv = document.querySelectorAll('div.jumbotron > div:first-of-type')
+    // let manDiv = document.querySelectorAll('div.jumbotron *')
+    // let newDiv = document.createElement('div')
+    // newDiv.classList.add('col-12')
+    // manDiv.classList.add('col-12')
+    let h1 = document.querySelector('h1')
+    h1.classList.add('col-6')
+    // newDiv.appendChild(h1)
+    // manDiv[2].appendChild(newDiv)
+    // manDiv.appendChild(newDiv)
+}
+
+// EX16) Write a function to remove the "Search" magnifying glass icon
+const removeSearch = function () {
+    let searchNode = document.querySelector('header div > div:nth-of-type(3) a:nth-of-type(1)')
+    searchNode.hidden = true
+}
+
+// EX17) Write a function to trim just the first 50 characters in the first paragraph for each blog post
+const trimCharacters = function () {
+    let p = document.querySelectorAll('main div div.blog-post > p:nth-of-type(2)')
+    for (let i = 0; i < p.length; i++) {
+        let str = p[i].innerText
+        let slic = str.slice(50)
+        console.log(slic)
+        slic
+    }
+}
+
+// EX18) Write a function and attach it to the "Newer" button, to add new Blog Post(just div and title)
+const newPost = function () {
+    let blogNode = document.querySelector('.blog-main .blog-post')
+
+    let nPost = document.createElement('div')
+    nPost.classList.add('col-md-8')
+    nPost.innerHTML += `
+                        <h1>New Post</h1>
+                        <p class="blog-post-meta">
+            August 30, 2021 by Zee
+          </p>
+                    
+                            `
+
+    blogNode.prepend(nPost)
+}
+
+// EX19) Write a function and attach it to the "Older" button, to remove the last Blog Post
+
+
+
 window.onload = function () {
+    trimCharacters()
+    changeColSize()
     addnewlinkToNav()
     changeColorOfMainTitle()
     jumbotronBackground()
     removelinksElsewhere()
+    removeSearch()
 }
